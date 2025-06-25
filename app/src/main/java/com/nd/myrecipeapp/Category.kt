f3bc1a84380853c037e994f8adda32ed9bbfeb04
Nikunj-Iguru <nikunj@iguru-india.com>
2025-06-25 14:39:44 +0530

    Added spacing

--- app/src/main/java/com/nd/myrecipeapp/Category.kt
+++ app/src/main/java/com/nd/myrecipeapp/Category.kt
@@ -5,5 +5,5 @@ data class Category(val idCategory: String,
                     val strCategoryThumb: String,
                     val strCategoryDescription: String)
 
-data class CategoryResponse(val categories: List<Category>)
+data class CategoryResponse (val categories: List<Category>)
 
