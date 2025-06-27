d6417f9b2e00057cb6dd200ea231e943db7a0c22
Nikunj-Iguru <nikunj@iguru-india.com>
2025-06-27 17:17:54 +0530

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
 
     //Image loading
     implementation("io.coil-kt:coil-compose:2.4.0")
-
+implementation("androidx.navigation:navigation-compose:2.7.4")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/nd/myrecipeapp/RecipeScreen.kt
+++ app/src/main/java/com/nd/myrecipeapp/RecipeScreen.kt
@@ -1,8 +1,10 @@
 package com.nd.myrecipeapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRowScopeInstance.align
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -24,7 +26,9 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 
 @Composable
-fun RecipeScreen(modifier: Modifier = Modifier) {
+fun RecipeScreen(modifier: Modifier = Modifier,
+                  navigatetodetail: (Category) -> Unit
+) {
     val recipeViewModel: MainViewModel = viewModel()
     val viewstate by recipeViewModel.categorieState
     Box(modifier = modifier.fillMaxSize()){
@@ -36,7 +40,7 @@ fun RecipeScreen(modifier: Modifier = Modifier) {
                 Text(text = "Error Occured")
         }
             else -> {
-                CategoryScreen(categories = viewstate.list)
+                CategoryScreen(categories = viewstate.list,navigatetodetail)
 
             }
         }
@@ -45,20 +49,25 @@ fun RecipeScreen(modifier: Modifier = Modifier) {
 }
 
 @Composable
-    fun CategoryScreen(categories: List<Category>){
+    fun CategoryScreen(categories: List<Category>,
+    navigatetodetail: (Category) -> Unit
+    ){
         LazyVerticalGrid(GridCells.Fixed(2),Modifier.fillMaxSize()){
 items(categories){
     category ->
-    CategoryItem(category = category)
+    CategoryItem(category = category, navigatetodetail)
            }
         }
     }
 
 //Hoe each item looks like
 @Composable
-fun CategoryItem(category: Category){
+fun CategoryItem(category: Category
+                 navigatetodetail: (Category) -> Unit
+){
     Column(modifier = Modifier.padding(8.dp).
-    fillMaxSize(),
+          fillMaxSize()
+        .clickable { navigatetodetail(category) },
         horizontalAlignment = Alignment.CenterHorizontally) {
         Image(
             painter = rememberAsyncImagePainter(category.strCategoryThumb),
